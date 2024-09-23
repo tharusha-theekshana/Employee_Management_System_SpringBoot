a769165c1bb42fde61c3bb8c8d64aa6f5632291c
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-09-23 08:47:45 +0530

    Add cross origin implementation to security.

--- src/main/java/com/emp/employee_management_system/Configurations/SecurityConfig.java
+++ src/main/java/com/emp/employee_management_system/Configurations/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -25,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET,"api/v1/auth/*").permitAll()
@@ -40,5 +43,19 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:61291"); // Your frontend URL
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // Allow all HTTP methods
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
